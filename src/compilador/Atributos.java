e0316e1108bdd54ea918199079c29faeb8012f7b
MSI <MSI@DESKTOP-GVERSTF>
2024-05-08 07:27:37 -0600

    20240508 Clases Adicionales

---
+++ src/compilador/Atributos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compilador;
+
+/**
+ *
+ * @author MSI
+ */
+public class Atributos {
+    String tipo;
+    String h;
+}
